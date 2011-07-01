0255eb1c7713ecdc02356b07f2074dc461178814
dtvernon <dtvernon@95015238-8795-11de-a133-9d545b4d7674>
2011-07-01 21:57:21 +0000

    tagged 3.2.0.2

---
+++ tags/3.2.0.2/plugin/loader/jni/loader.c
@@ -0,0 +1,217 @@
+#include "angdroid.h"
+
+#include <jni.h>
+#include <android/log.h>
+#include <pthread.h>
+#include <dlfcn.h>
+#include <stdio.h>
+
+#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, "Angband", __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG  , "Angband", __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO   , "Angband", __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN   , "Angband", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR  , "Angband", __VA_ARGS__) 
+
+/* FIXME __android_log_write or __android_log_print ??? */
+#define LOG(msg) (__android_log_write(ANDROID_LOG_DEBUG, "Angband", (msg)));
+
+void( * angdroid_gameStart ) (JNIEnv*, jobject, jint, jobjectArray) = NULL; 
+jint( * angdroid_gameQueryRedraw ) (JNIEnv*, jobject, jint, jint, jint, jint) = NULL;
+jint( * angdroid_gameQueryInt ) (JNIEnv*, jobject, jint, jobjectArray) = NULL;
+jstring( * angdroid_gameQueryString ) (JNIEnv*, jobject, jint, jobjectArray) = NULL;
+
+JNIEnv* pass_env1; 
+jobject pass_obj1;
+jint    pass_argc;
+jobject pass_argv;
+
+static JavaVM *jvm;
+static void* handle = NULL;
+pthread_mutex_t muQuery = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t muGame = PTHREAD_MUTEX_INITIALIZER;
+
+static jclass NativeWrapperClass;
+static jobject NativeWrapperObj;
+static jmethodID NativeWrapper_onGameExit;
+
+void* run_angband(void* foo)
+{
+	angdroid_gameStart(pass_env1, pass_obj1, pass_argc, pass_argv);   
+	return foo;
+}
+
+void gameStart
+(JNIEnv *env1, jobject obj1, jstring pluginPath, jint argc, jobjectArray argv)
+{
+	pthread_t game_thread;
+
+	//LOGD("loader.startGame.syncwait");
+
+	// begin synchronize
+
+	int ct = 0;
+	while (pthread_mutex_trylock(&muGame)!=0) {
+		sleep(100);
+		if(ct++>5) {
+			LOGE("failed to acquire game thread lock, bailing");
+			return;
+		}
+	}
+
+	pthread_mutex_lock (&muQuery);
+
+	//LOGD("loader.startGame.initializing");
+
+	pass_env1 = env1;
+	pass_obj1 = obj1;
+	pass_argc = argc;
+	pass_argv = argv;
+
+	/* Init exit game callback */
+	NativeWrapperObj = obj1;
+	NativeWrapperClass = (*env1)->GetObjectClass(env1, NativeWrapperObj);
+	NativeWrapper_onGameExit = (*env1)->GetMethodID(env1, NativeWrapperClass, "onGameExit", "()V");
+
+	// load game plugin lib
+	const char *copy_pluginPath = (*env1)->GetStringUTFChars(env1, pluginPath, 0);
+	//LOGD("loader.dlopen %s", copy_pluginPath);
+	handle = dlopen(copy_pluginPath,RTLD_LOCAL | RTLD_LAZY);  
+	if (!handle) {
+		LOGE("dlopen failed on %s", copy_pluginPath);
+		return;
+	}	
+	(*env1)->ReleaseStringUTFChars(env1, pluginPath, copy_pluginPath);
+
+	// find entry point
+	angdroid_gameStart = dlsym(handle, "angdroid_gameStart");   
+	if (!angdroid_gameStart) {
+		LOGE("dlsym failed on gameStart");
+		return;
+	}	
+
+	//LOGD("loader.create_thread");
+	// start thread to call entry point
+	pthread_create(&game_thread, NULL, run_angband, (void*) NULL);	
+
+	// end synchronize
+	pthread_mutex_unlock (&muQuery);
+
+	LOGD("loader.waiting on game_thread");
+	pthread_join(game_thread, NULL); // wait for thread to exit
+
+	// begin synchronize
+	pthread_mutex_lock (&muQuery);
+
+	LOGD("loader.game_thread is finished");
+	dlclose(handle);           	 // unload angband lib
+
+	// clear pointers
+	handle = NULL;
+	angdroid_gameQueryInt = NULL;
+	angdroid_gameQueryString = NULL;
+
+	// signal game has exited
+	(*env1)->CallVoidMethod(env1, NativeWrapperObj, NativeWrapper_onGameExit);
+
+	//LOGD("loader.unlock mutexes");
+	// end synchronize
+	pthread_mutex_unlock (&muQuery);
+	pthread_mutex_unlock (&muGame);
+
+	//LOGD("loader.return");
+}
+
+JNIEXPORT void JNICALL Java_org_angdroid_angband_NativeWrapper_gameStart
+(JNIEnv *env1, jobject obj1, jstring pluginPath, jint argc, jobjectArray argv)
+{
+	gameStart(env1,obj1,pluginPath,argc,argv);
+}
+
+JNIEXPORT void JNICALL Java_org_angdroid_variants_NativeWrapper_gameStart
+(JNIEnv *env1, jobject obj1, jstring pluginPath, jint argc, jobjectArray argv)
+{
+	gameStart(env1,obj1,pluginPath,argc,argv);
+}
+
+JNIEXPORT void JNICALL Java_org_angdroid_classics_NativeWrapper_gameStart
+(JNIEnv *env1, jobject obj1, jstring pluginPath, jint argc, jobjectArray argv)
+{
+	gameStart(env1,obj1,pluginPath,argc,argv);
+}
+
+JNIEXPORT void JNICALL Java_org_angdroid_nightly_NativeWrapper_gameStart
+(JNIEnv *env1, jobject obj1, jstring pluginPath, jint argc, jobjectArray argv)
+{
+	gameStart(env1,obj1,pluginPath,argc,argv);
+}
+
+JNIEXPORT jstring JNICALL Java_org_angdroid_angband_NativeWrapper_gameQueryString
+  (JNIEnv *env1, jobject obj1, jint argc, jobjectArray argv)
+{
+	return (jstring)0; // null indicates error
+}
+JNIEXPORT jstring JNICALL Java_org_angdroid_variants_NativeWrapper_gameQueryString
+  (JNIEnv *env1, jobject obj1, jint argc, jobjectArray argv)
+{
+	return (jstring)0; // null indicates error
+}
+JNIEXPORT jstring JNICALL Java_org_angdroid_classics_NativeWrapper_gameQueryString
+  (JNIEnv *env1, jobject obj1, jint argc, jobjectArray argv)
+{
+	return (jstring)0; // null indicates error
+}
+JNIEXPORT jstring JNICALL Java_org_angdroid_nightly_NativeWrapper_gameQueryString
+  (JNIEnv *env1, jobject obj1, jint argc, jobjectArray argv)
+{
+	return (jstring)0; // null indicates error
+}
+
+jint gameQueryInt
+(JNIEnv *env1, jobject obj1, jint argc, jobjectArray argv)
+{
+	jint result = -1; // -1 indicates error
+
+	// begin synchronize
+	pthread_mutex_lock (&muQuery);
+
+	if (handle) {
+		if (!angdroid_gameQueryInt)
+		  	// find entry point
+		  	angdroid_gameQueryInt = dlsym(handle, "angdroid_gameQueryInt");   
+
+		if (angdroid_gameQueryInt)
+			result = angdroid_gameQueryInt(env1, obj1, argc, argv);
+		else
+			LOGE("dlsym failed on angdroid_gameQueryInt");
+	}
+	else {
+		LOGE("dlopen failed -- angdroid_gameQueryInt");
+	}
+
+	// end synchronize
+	pthread_mutex_unlock (&muQuery);
+
+	return result;
+}
+
+JNIEXPORT jint JNICALL Java_org_angdroid_angband_NativeWrapper_gameQueryInt
+(JNIEnv *env1, jobject obj1, jint argc, jobjectArray argv)
+{
+	return gameQueryInt(env1,obj1,argc,argv);
+}
+JNIEXPORT jint JNICALL Java_org_angdroid_variants_NativeWrapper_gameQueryInt
+(JNIEnv *env1, jobject obj1, jint argc, jobjectArray argv)
+{
+	return gameQueryInt(env1,obj1,argc,argv);
+}
+JNIEXPORT jint JNICALL Java_org_angdroid_classics_NativeWrapper_gameQueryInt
+(JNIEnv *env1, jobject obj1, jint argc, jobjectArray argv)
+{
+	return gameQueryInt(env1,obj1,argc,argv);
+}
+JNIEXPORT jint JNICALL Java_org_angdroid_nightly_NativeWrapper_gameQueryInt
+(JNIEnv *env1, jobject obj1, jint argc, jobjectArray argv)
+{
+	return gameQueryInt(env1,obj1,argc,argv);
+}
+
---
+++ tags/3.2.0.2/src/org/angdroid/angband/Plugins.java
@@ -0,0 +1,102 @@
+package org.angdroid.angband;
+
+import java.util.zip.ZipInputStream;
+import java.util.Scanner;
+import java.io.InputStream;
+import android.os.Environment;
+
+final public class Plugins {
+	public enum Plugin {
+		Angband(0), Angband306(1), FrogKnows(2);
+		private int id;
+
+		private Plugin(int id) {
+			this.id = id;
+		}
+		public int getId() {
+			return id;
+		}
+		public static Plugin convert(int value) {
+			return Plugin.class.getEnumConstants()[value];
+		}
+	}
+
+	static final String DEFAULT_PROFILE = "0~Default~PLAYER~0~0|0~Borg~BORGSAVE~1~1";
+	public static String LoaderLib ="loader-angband";
+
+	public static String getFilesDir(Plugin p) {
+		switch (p) {
+		case Angband: return "angband320";
+		case Angband306: return "306";
+		default: return p.toString().toLowerCase();
+		}
+	}
+
+	public static int getKeyDown(Plugin p) {
+		switch (p) {
+		case Angband: return 0x8A;
+		default: return '2';
+		}
+	}
+
+	public static int getKeyUp(Plugin p) {
+		switch (p) {
+		case Angband: return 0x8D;
+		default: return '8';
+		}
+	}
+
+	public static int getKeyLeft(Plugin p) {
+		switch (p) {
+		case Angband: return 0x8B;
+		default: return '4';
+		}
+	}
+
+	public static int getKeyRight(Plugin p) {
+		switch (p) {
+		case Angband: return 0x8C;
+		default: return '6';
+		}
+	}
+
+	public static ZipInputStream getPluginZip(int plugin) {
+		InputStream is = null;
+		if (plugin == Plugin.Angband.getId())
+			is = Preferences.getResources().openRawResource(R.raw.zipangband320);
+		else if (plugin == Plugin.Angband306.getId())
+			is = Preferences.getResources().openRawResource(R.raw.zipangband306);
+		else if (plugin == Plugin.FrogKnows.getId())
+			is = Preferences.getResources().openRawResource(R.raw.zipfrogknows);
+		return new ZipInputStream(is);
+	}
+	public static String getPluginCrc(int plugin) {
+		InputStream is = null;
+		if (plugin == Plugin.Angband.getId())
+			is = Preferences.getResources().openRawResource(R.raw.crcangband320);
+		else if (plugin == Plugin.Angband306.getId())
+			is = Preferences.getResources().openRawResource(R.raw.crcangband306);
+		else if (plugin == Plugin.FrogKnows.getId())
+			is = Preferences.getResources().openRawResource(R.raw.crcfrogknows);
+		return new Scanner(is).useDelimiter("\\A").next().trim();
+	}
+
+	public static String getUpgradePath(Plugin p) {
+		switch (p) {
+		case Angband: 
+			return Environment.getExternalStorageDirectory()
+				+ "/"
+				+ "Android/data/org.angdroid.angband/files/lib/save";
+		default: return "";
+		}		
+	}
+
+	public static String getStartBorgSequence() {
+		Plugin p = Preferences.getActivePlugin();
+		switch(p) {
+		case Angband: return "```=g22222y``\r\r`\r\r`\r2\r\r*\r\r```\032  y`\032z";
+		case Angband306: return "```=722222y``\r\r\r\rX\r\r\032  y`\032z";
+		default: return "";
+		}
+	}
+}
