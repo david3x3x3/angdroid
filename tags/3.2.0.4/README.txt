fb7354585480001ea39345b53a4653f8df142cba
david20708@gmail.com <david20708@gmail.com@95015238-8795-11de-a133-9d545b4d7674>
2011-08-18 04:55:49 +0000

    tagged 3.2.0.4

---
+++ tags/3.2.0.4/README.txt
@@ -0,0 +1,27 @@
+How to compile the Angband Native port for Android.
+
+To build, you should use Ant 1.8.1. See here http://ant.apache.org/.
+
+1. Edit build.properties file and set ndk-location and sdk-location properties:
+
+...
+ndk-location=/home/user/Java/android-ndk-r5b
+sdk-location=/home/user/Java/android-sdk-linux
+...
+
+The ndk-location and sdk-location properties should point to Android NDK and Android SDK.
+
+
+2. To build the Angband debug apk file, execute:
+
+ant
+
+-------------------------------------------------------------------
+
+Other supported targets for Angband Variants apk and release builds:
+
+ant angband-release
+ant variants-debug
+ant variants-release
+
+Note that Angband Variants apks end up under angdroid/alt-apk/variants/bin/
---
+++ tags/3.2.0.4/alt-apk/override/variants/src/org/angdroid/variants/Plugins.java
@@ -0,0 +1,97 @@
+package org.angdroid.variants;
+
+import java.util.zip.ZipInputStream;
+import java.util.Scanner;
+import java.io.InputStream;
+
+final public class Plugins {
+	public enum Plugin {
+		ToME(0), Sangband(1), Steamband(2);
+		//NPP(?);
+
+		private int id;
+
+		private Plugin(int id) {
+			this.id = id;
+		}
+		public int getId() {
+			return id;
+		}
+		public static Plugin convert(int value) {
+			return Plugin.class.getEnumConstants()[value];
+		}
+	}
+
+	static final String DEFAULT_PROFILE = "0~Default~PLAYER~0~0";
+	public static String LoaderLib ="loader-variants";
+
+	public static String getFilesDir(Plugin p) {
+		switch (p) {
+		case Sangband: return "sang";
+		case Steamband: return "steam";
+		default: return p.toString().toLowerCase();
+		}
+	}
+
+	public static int getKeyDown(Plugin p) {
+		switch (p) {
+		default: return '2';
+		}
+	}
+
+	public static int getKeyUp(Plugin p) {
+		switch (p) {
+		default: return '8';
+		}
+	}
+
+	public static int getKeyLeft(Plugin p) {
+		switch (p) {
+		default: return '4';
+		}
+	}
+
+	public static int getKeyRight(Plugin p) {
+		switch (p) {
+		default: return '6';
+		}
+	}
+
+	public static ZipInputStream getPluginZip(int plugin) {
+		InputStream is = null;
+
+		if (plugin == Plugin.ToME.getId())
+			is = Preferences.getResources().openRawResource(R.raw.ziptome);
+		else if (plugin == Plugin.Sangband.getId())
+			is = Preferences.getResources().openRawResource(R.raw.zipsang);
+		else if (plugin == Plugin.Steamband.getId())
+			is = Preferences.getResources().openRawResource(R.raw.zipsteam);
+		//else if (plugin == Plugin.NPP.getId())
+		//	is = Preferences.getResources().openRawResource(R.raw.zipnpp);
+
+		return new ZipInputStream(is);
+	}
+	public static String getPluginCrc(int plugin) {
+		InputStream is = null;
+		if (plugin == Plugin.ToME.getId())
+			is = Preferences.getResources().openRawResource(R.raw.crctome);
+		else if (plugin == Plugin.Sangband.getId())
+			is = Preferences.getResources().openRawResource(R.raw.crcsang);
+		else if (plugin == Plugin.Steamband.getId())
+			is = Preferences.getResources().openRawResource(R.raw.crcsteam);
+		//else if (plugin == Plugin.NPP.getId())
+		//	is = Preferences.getResources().openRawResource(R.raw.crcnpp);
+		return new Scanner(is).useDelimiter("\\A").next().trim();
+	}
+
+	public static String getUpgradePath(Plugin p) {
+		switch (p) {
+		default: return "";
+		}		
+	}
+
+	public static String getStartBorgSequence() {
+		return "";
+	}
+
+}
